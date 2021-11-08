de89d6766b56184732fc01b97c40a5b5d35e625a
MinSeo <86696817+gyullo18@users.noreply.github.com>
2021-11-08 16:31:34 +0900

    11/8 rest_api 연습

---
+++ product/models.py
@@ -0,0 +1,15 @@
+# import json
+# from django.db import models
+# 모델 필드 다시...
+# class labeling(models.Model):
+#         version = models.IntegerField()
+#         flags = models.CharField(data, max_length=1000,null=True)
+#         shape = models.CharField(data, max_length=1000, null=False)
+#         imagePath = models.ImageField()
+#         imageData = models.TextField()
+#         imageHeight = models.IntegerField()
+#         imageWidth = models.IntegerField()
+#
+#         def __str__(self):
+#             return self.shape
+#
---
+++ product/serializer.py
@@ -0,0 +1,8 @@
+# from rest_framework import serializers
+# from . import models
+# 모델 완성 후 다시 확인
+#
+# class labelingserializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = models.labeling
+#         fields = ['shape', imageData']
